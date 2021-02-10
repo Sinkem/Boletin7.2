4c118e585a0eb8147f37a36a30f10515492f3b8f
Sinkem <sinkemredes@gmail.com>
2021-02-10 21:15:46 +0100

    commit ya no se cual

--- Ejercicio6/Carta.java
+++ Ejercicio6/Carta.java
@@ -11,6 +11,24 @@ public class Carta {
     public Carta(double sumaTotal) {
         this.sumaTotal = sumaTotal;
 
+        int numeroPalo = (int) Math.random() * 3 + 1;
+        Palo palo;
+
+        switch (numeroPalo) {
+            case 1:
+                Palo.valueOf("COPAS");
+                break;
+            case 2:
+                Palo.valueOf("ESPADAS");
+                break;
+            case 3:
+                Palo.valueOf("BASTOS");
+                break;
+            case 4:
+                Palo.valueOf("OROS");
+                break;
+        }
+
     }
 
 }
---
+++ Ejercicio6/Main.java
@@ -0,0 +1,10 @@
+package Ejercicio6;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Carta carta = new Carta(5.2);
+
+        System.out.println(carta.);
+    }
+}
